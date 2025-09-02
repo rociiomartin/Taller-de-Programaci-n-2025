4e39ef988abdf957aff36c013a824531bb9ef917
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2025-09-02 16:42:13 -0300

    Create Ejercicio2.pas

---
+++ Imperativo/Clase 2/Ejercicio2.pas
@@ -0,0 +1,13 @@
+{
+Escribir un programa que: 
+a. Implemente un módulo recursivo que genere y retorne una lista de números enteros 
+“random” en el rango 100-200. Finalizar con el número 100. 
+b. Un módulo recursivo que reciba la lista generada en a) e imprima los valores de la lista en el 
+mismo orden que están almacenados. 
+c. Implemente un módulo recursivo que reciba la lista generada en a) e imprima los valores de 
+la lista en orden inverso al que están almacenados. 
+d. Implemente un módulo recursivo que reciba la lista generada en a) y devuelva el mínimo 
+valor de la lista.  
+e. Implemente un módulo recursivo que reciba la lista generada en a) y un valor y devuelva 
+verdadero si dicho valor se encuentra en la lista o falso en caso contrario. 
+}
