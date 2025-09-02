fb1926b7a388400d1101a9ccf704bbfbb1b06324
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2025-09-02 16:44:05 -0300

    Create Ejercicio3.pas

---
+++ Imperativo/Clase 2/Ejercicio3.pas
@@ -0,0 +1,12 @@
+{
+Implementar un programa que invoque a los siguientes módulos. 
+a. Un módulo recursivo que retorne un vector de 20 números enteros “random” mayores a 300 
+y menores a 1550 (incluidos ambos).  
+b. Un módulo que reciba el vector generado en a) y lo retorne ordenado. (Utilizar lo realizado 
+en la práctica anterior) 
+c. Un módulo que realice una búsqueda dicotómica en el vector, utilizando el siguiente 
+encabezado: 
+Procedure busquedaDicotomica (v: vector; ini,fin: indice; dato:integer; var pos: indice);  
+Nota: El parámetro “pos” debe retornar la posición del dato o -1 si el dato no se encuentra 
+en el vector.
+}
